b90b53eb161ca9203a77f301abe7dda30efbb776
AmosRibeiro <amosribeiro2708@gmail.com>
2024-07-17 17:55:38 -0300

    explicação da constante

--- tipos-variaveis/src/App.java
+++ tipos-variaveis/src/App.java
@@ -7,6 +7,10 @@ public class App {
     int numeroNormal = numeroCurto;
     //casting
     short numeroCurto2 = (short) numeroNormal;
+    //constante precisa ter final declarado e é usada em caixa alta
+    final int NUMERO = 10;
+
+    System.out.println(NUMERO);
 
     System.out.println(numeroCurto2);
     }
