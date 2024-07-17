588c80bc6fe231efb9299344a65efba79dff2994
AmosRibeiro <amosribeiro2708@gmail.com>
2024-07-17 17:35:34 -0300

    pequena mudança

--- tipos-variaveis/src/App.java
+++ tipos-variaveis/src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        
+        // TiposEVariaveis.java
+    short numeroCurto = 1;
+    int numeroNormal = numeroCurto;
+    //casting
+    short numeroCurto2 = (short) numeroNormal;
+
+    System.out.println(numeroCurto2);
     }
 }
