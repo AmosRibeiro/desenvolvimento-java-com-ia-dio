06d57656ef4f8d97e506c987492e2ba564035e7d
AmosRibeiro <amosribeiro2708@gmail.com>
2024-07-18 16:01:30 -0300

    adição de ternarios

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -27,6 +27,26 @@
             variavel = !variavel;
 
             System.out.println(variavel);
+
+            //ternarios
+            // classe Operadores.java
+            int a, b;
+
+            a = 5;
+            b = 5;
+
+
+            /* EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE
+            if(a==b)
+            resultado = "verdadeiro";
+            else
+            resultado = "falso";
+            */
+
+            //MESMA CONDICIONAL, MAS DESSA VEZ, UTILIZANDO O OPERADOR CONDICIONAL TERNÁRIO
+            String resultado = (a==b) ? "verdadeiro" : "false";
+
+             System.out.println(resultado);
         }
 
 }
