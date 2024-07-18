4aab5f41a5addac50ec78f3333f72a3a4486448c
AmosRibeiro <amosribeiro2708@gmail.com>
2024-07-18 15:25:42 -0300

    estudo de operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,32 @@
+    public class Operadores {
+        public static void main(String[] args) {
+
+            //classe Operadores.java
+            String nomeCompleto = "LINGUAGEM" + " " + "JAVA";
+            
+            System.out.println(nomeCompleto);
+
+            int numero = 5;
+
+            numero = - numero;
+
+            System.out.println(- numero);
+            System.out.println(numero);
+            
+            numero = numero * -1;
+            //x repetição numero = numero +1;
+
+            numero++;
+
+            System.out.println(numero);
+
+            boolean variavel = true;
+
+            System.out.println(variavel);
+
+            variavel = !variavel;
+
+            System.out.println(variavel);
+        }
+
+}
