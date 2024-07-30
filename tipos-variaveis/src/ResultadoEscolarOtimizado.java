7f5f775d0dad1e02e19134bca32ad235fc113bf1
AmosRibeiro <amosribeiro2708@gmail.com>
2024-07-30 17:56:44 -0300

    atualização do codigo atualização do codigo

--- tipos-variaveis/src/ResultadoEscolarOtimizado.java
+++ tipos-variaveis/src/ResultadoEscolarOtimizado.java
@@ -1,8 +1,8 @@
 public class ResultadoEscolarOtimizado {
     public static void main(String[] args) {
-        int nota = 7;
+        int nota = 3;
 
-        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+        String resultado = nota >= 7 ? "Aprovado" :  nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado";
 
         System.out.println(resultado);
     }
