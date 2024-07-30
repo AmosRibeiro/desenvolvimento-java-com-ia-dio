6a35f8a13c57a555b9751f05b0ff493f43d09373
AmosRibeiro <amosribeiro2708@gmail.com>
2024-07-30 15:03:41 -0300

    exemplo de documentação

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"App"},{"p":"<Unnamed>","l":"Calculadora"},{"p":"<Unnamed>","l":"Operadores"},{"p":"<Unnamed>","l":"SmartTV"},{"p":"<Unnamed>","l":"UsuarioSmartTV"}];updateSearchResults();
